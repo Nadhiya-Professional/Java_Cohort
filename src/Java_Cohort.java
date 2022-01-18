348ac3058f2baea4747da88c78dba4b33feacd7b
Nadhiya-Professional <nadhiya.b@gmail.com>
2022-01-18 11:35:41 -0600

    Initial commit

---
+++ src/Java_Cohort.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class Java_Cohort {
+            private final int a; // variable 'a' defined in the default constructor.
+            private final int b ;
+            
+            //In default constructor the instance variables are set. These variables can be accessed by the methods with in this class.
+            // this is used to refer to current object or instance of the class. This is mainly used to differentiate between the class variables
+            // and the attributes that are passed in the constructor, if they have same name.
+
+            public Java_Cohort(int a,int b) {
+                this.a = a;
+                this.b = b;
+            }
+
+            public void addition() {
+                int c = a + b;
+                System.out.println("c is :" + c);
+            }
+
+            public void subtraction() {
+                int c = a - b;
+                System.out.println("c is :" + c);
+            }
+
+            public void multiplication() {
+                int c = a * b;
+                System.out.println("c is :" + c);
+            }
+
+            public void division() {
+                int c = a / b;
+                System.out.println("c is :" + c);
+            }
+
+        public static void main(String args[]){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Please enter the first number to do the arithmetic operations:");
+            int a = in.nextInt();
+            System.out.println("Please enter the second number:");
+            int b = in.nextInt();
+            Java_Cohort jc = new Java_Cohort(a,b); // default constructor is called whenever the object is created.
+            jc.addition(); // using the object of the class operation we are accessing the methods of the class.
+            jc.subtraction();
+            jc.multiplication();
+            jc.division();
+        }
+    }
+
+
+
+
+
+
